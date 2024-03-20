6bc939b8ef2e210e3f16f10389487996a37c0317
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2024-03-20 17:13:43 +0100

    [pandas_read] -add read_multiple_csv_from_dir + tests

--- src/akutils/__api__.py
+++ src/akutils/__api__.py
@@ -4,7 +4,8 @@ from akutils.pandas_type_conversion import (
     columns_to_date
 )
 from akutils.pandas_read_files import (
-    read_csv_in_chunks
+    read_csv_in_chunks,
+    read_multiple_csv_from_dir
 )
 from akutils.pandas_serie_cleaner import (
     strip_columns,
--- src/akutils/tests/unit/test_pandas_read_files.py
+++ src/akutils/tests/unit/test_pandas_read_files.py
@@ -73,5 +73,82 @@ class TestReadCsvInChunks():
         pd.testing.assert_frame_equal(df, df_expected)
 
 
+class TestReadMultipleCsvFromDir():
+
+    df_expected = pd.DataFrame(
+        data=[
+            [1, 1, "Italy"],
+            [1, 2, "Germany"],
+            [1, 3, "France"],
+            [2, 4, "Italy"],
+            [3, 5, "Germany"],
+            [3, 6, "France"]
+        ],
+        columns=["month", "nb_sales", "country"]
+    )
+
+    def test_read_multiple_csv_from_dir(self):
+        """
+        Simple cases (the dir containing also not text file extension)
+        """
+        df_expected = self.df_expected.copy()
+        dir_path = PATH_TO_AKUTILS_PKG / "tests" / "_fixtures" / "sales_per_month"
+        df = ak.read_multiple_csv_from_dir(dir_path, sep=";", chunksize=5, dtype=None)
+        df = df.sort_values("nb_sales").reset_index(drop=True)
+        pd.testing.assert_frame_equal(df, df_expected)
+
+    def test_read_multiple_csv_from_dir_with_filter_files(self):
+        """
+        Case with file name filter (the dir containing also not text file extension)
+        """
+        df_expected = self.df_expected.copy()
+        df_expected = df_expected[df_expected["month"] == 1]
+        dir_path = PATH_TO_AKUTILS_PKG / "tests" / "_fixtures" / "sales_per_month"
+        df = ak.read_multiple_csv_from_dir(
+            dir_path,
+            regex="^Sales_01",
+            sep=";",
+            dtype=None)
+        df = df.sort_values("nb_sales").reset_index(drop=True)
+        pd.testing.assert_frame_equal(df, df_expected)
+
+    def test_read_multiple_csv_from_dir_with_filter_files_case_sensitive(self):
+        """
+        Case with file case sensitive name filter (the dir containing also not text
+        file extension)
+        """
+        df_expected = self.df_expected.copy()
+        df_expected = df_expected[df_expected["month"] == 3].reset_index(drop=True)
+        dir_path = PATH_TO_AKUTILS_PKG / "tests" / "_fixtures" / "sales_per_month"
+        df = ak.read_multiple_csv_from_dir(
+            dir_path,
+            regex="^Sales_",
+            case_sensitive=True,
+            sep=";",
+            dtype=None)
+        df = df.sort_values("nb_sales")
+        pd.testing.assert_frame_equal(df, df_expected)
+
+    def test_read_multiple_csv_from_dir_with_chunk_function(self):
+        """
+        Case with chunk function (the dir containing also not text file extension)
+        """
+
+        def filter_chunk(df_chunk: pd.DataFrame) -> pd.DataFrame:
+            return df_chunk[df_chunk["nb_sales"] <= 4]
+
+        df_expected = self.df_expected.copy()
+        df_expected = df_expected[df_expected["nb_sales"] <= 4]
+        dir_path = PATH_TO_AKUTILS_PKG / "tests" / "_fixtures" / "sales_per_month"
+        df = ak.read_multiple_csv_from_dir(
+            dir_path,
+            chunk_func=filter_chunk,
+            chunksize=2,
+            sep=";",
+            dtype=None)
+        df = df.sort_values("nb_sales").reset_index(drop=True)
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
