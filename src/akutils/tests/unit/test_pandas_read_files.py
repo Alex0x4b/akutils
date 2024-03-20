e4b1b8ec876b0c66cda3530ce4253d7c5cc95096
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2024-03-20 09:35:30 +0100

    [pandas_read] -add test with chunk new col function for read_csv_in_chunk

--- src/akutils/tests/unit/test_pandas_read_files.py
+++ src/akutils/tests/unit/test_pandas_read_files.py
@@ -42,6 +42,36 @@ class TestReadCsvInChunks():
         )
         pd.testing.assert_frame_equal(df, df_expected)
 
+    def test_read_csv_in_chunk_with_add_cols_function(self):
+        """
+        Read with new col function apply on each chunk function
+        """
+        file_path = PATH_TO_AKUTILS_PKG / "tests" / "_fixtures" / "sales.csv"
+        df_expected = pd.read_csv(file_path, sep=";")
+        df_expected["new_col"] = (df_expected["col1"] + df_expected["col2"]) * 2.4 + 151
+
+        def add_new_col_to_chunk(
+            df_chunk: pd.DataFrame,
+            factor: float,
+            constant: int
+        ) -> pd.DataFrame:
+            df_chunk["new_col"] = (
+                (df_chunk["col1"] + df_chunk["col2"]) * factor + constant
+            )
+            return df_chunk
+
+        file_path = PATH_TO_AKUTILS_PKG / "tests" / "_fixtures" / "sales.csv"
+        df = ak.read_csv_in_chunks(
+            file_path,
+            chunk_func=add_new_col_to_chunk,
+            factor=2.4,
+            constant=151,
+            chunksize=5,
+            sep=";",
+            dtype=None
+        )
+        pd.testing.assert_frame_equal(df, df_expected)
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
