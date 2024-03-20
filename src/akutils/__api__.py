5fd6bdef1034d955e89fc7aba79e134405a0272f
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2024-03-20 12:07:54 +0100

    [serie_cleaner] -add strip_columns + tests

--- src/akutils/__api__.py
+++ src/akutils/__api__.py
@@ -2,3 +2,4 @@ from akutils.pandas_type_conversion import columns_to_float
 from akutils.pandas_read_files import (
     read_csv_in_chunks
 )
+from akutils.pandas_serie_cleaner import strip_columns
--- src/akutils/pandas_serie_cleaner.py
+++ src/akutils/pandas_serie_cleaner.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas.api.types import is_string_dtype, is_object_dtype
+from typing import Optional
 import warnings
 
 
@@ -44,7 +46,7 @@ def remove_accent_from_cols(
     df : pd.DataFrame
         DataFrame to be modified
     cols : list
-        List of column names for which special accents and brackets must be removed
+        List of column names for which special accents and brackets should be removed
     """
     for col in cols:
         if col not in df.columns:
@@ -58,4 +60,29 @@ def remove_accent_from_cols(
             .str.encode('ascii', errors='ignore')
             .str.decode('utf-8')
         )
+    return df
+
+
+def strip_columns(
+    df: pd.DataFrame,
+    cols: Optional[list] = None
+) -> pd.DataFrame:
+    """
+    Strip selected DataFrame columns.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame to be modified
+    cols : list, default None
+        List of column names to be striped
+    """
+    cols = list(df.columns) if not cols else cols
+    for column in cols:
+        if column not in df.columns:
+            warnings.warn(f"column not found in DataFrame: {column}")
+            continue
+        if (not is_string_dtype(df[column])) & (not is_object_dtype(df[column])):
+            continue
+        df.loc[(~df[column].isna()), column] = df[column].str.strip()
     return df
