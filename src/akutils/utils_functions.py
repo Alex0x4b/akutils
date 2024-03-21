b4afdba2e98ba47dfea4f3d451e5e28440ff4694
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2024-03-21 16:14:45 +0100

    [utils_function] -refactore sanitize_funct_arg

--- src/akutils/utils_functions.py
+++ src/akutils/utils_functions.py
@@ -19,17 +19,18 @@ def sanitize_function_args_from_locals(function, locals_args):
     if not hasattr(function, '__call__'):
         return dict()
 
-    # Contruct agruments from local
-    specified_args = {
-        key: value for key, value in locals_args.items()
-        if key not in ["kwargs"]
-    }
-    additionnal_kwargs = locals_args["kwargs"]
-    all_args = dict(specified_args, **additionnal_kwargs)
+    flatten_argument = dict()
+    if "kwargs" in locals_args.keys():
+        flatten_argument.update(locals_args["kwargs"])
 
-    # Filter on function alloewed args
+    flatten_argument.update({
+        key: locals_args[key] for key in locals_args.keys()
+        if key not in ["kwargs"]
+    })
+
+    # Filter on function allowed args
     function_args = {
-        key: value for key, value in all_args.items()
+        key: flatten_argument[key] for key in flatten_argument.keys()
         if key in function.__code__.co_varnames
     }
     return function_args
