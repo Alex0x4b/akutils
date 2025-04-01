234d73e3a83e1259b230ad808e2bcd095ed9ae0d
Alex0x4b <76492657+Alex0x4b@users.noreply.github.com>
2025-04-01 17:39:25 +0200

    -add read_multiple_csv_from_zip in API

--- src/akutils/__api__.py
+++ src/akutils/__api__.py
@@ -6,7 +6,8 @@ from akutils.pandas_type_conversion import (
 from akutils.pandas_read_files import (
     read_csv_in_chunks,
     read_multiple_csv_from_dir,
-    read_multiple_xlsx_from_dir
+    read_multiple_xlsx_from_dir,
+    read_multiple_csv_from_zip,
 )
 from akutils.pandas_serie_cleaner import (
     strip_columns,
